49a444ac56262010b9b4f24813096741c267fe5a
Antal Bako <prowlersk8@gmail.com>
2021-04-19 17:07:58 +0200

    got the button to post,connect form data, and refresh after post!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,21 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
 
   def home
     gon.ip = request.remote_ip
     gon.ranks = Rank.all.order("score DESC")
     gon.work = Work.all
+  end
+
+  def create
+    @rank = Rank.new(rank_params)
+    @rank.save
+  end
+
+  private
+
+  def rank_params
+    params.permit(:score, :name, :country)
   end
 end
