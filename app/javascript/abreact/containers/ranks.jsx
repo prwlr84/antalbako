3dbf54f8a81d61e7943c89e6ba6c53344854ee65
Antal Bako <prowlersk8@gmail.com>
2021-04-20 16:55:08 +0200

    form connected, needs to fix reload still

--- app/javascript/abreact/containers/ranks.jsx
+++ app/javascript/abreact/containers/ranks.jsx
@@ -4,20 +4,32 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class Ranks extends Component {
+  constructor(props){
+    super(props);
+    this.state = { record: false };
+
+    this.record = this.record.bind(this);
+  }
+
   record(x){
-    console.log(this.props.ranks);
-    //if(this.props.score > x[x.length-1].score){
-      return(
-        <div>
-          <h1 id="ranksTitle">Congrats! That's in the best 10! Please, add your details to the hall of fame:</h1>
-          <form action="/pages?country=HUNGARY&name=Antal&score=10000" method="POST">
-            <input type="text" placeholder="Name"/>
-            <input type="text" placeholder="Country"/>
-            <input type="submit"/>
-          </form>
-        </div>
-      )
-    //}
+    function postrank(x,y){
+      const form = document.querySelector('#rankForm');
+      const name = document.querySelector('input[name="name"]').value;
+      const coun = document.querySelector('input[name="country"]').value;
+
+      form.action = `http://localhost:3000/pages?country=${coun}&name=${name}&score=${x}`;
+    }
+
+    return(
+      <div>
+        <h1 id="ranksTitle">Congrats! That's in the best 10! Please, add your details to the hall of fame:</h1>
+        <form id='rankForm' onSubmit={() => {postrank(this.props.score, this);this.setState({ refresh: !this.state.refresh })}} method='POST'>
+          <input type="text" name='name' placeholder="Name"/>
+          <input type="text" name='country' placeholder="Country"/>
+          <input type="submit"/>
+        </form>
+      </div>
+    )
   }
 
   render(){
@@ -25,10 +37,10 @@ class Ranks extends Component {
     return(
       <div className="ranks col-12 col-sm-6">
         <h2>{word[Math.floor(Math.random() * word.length)]}! You got {this.props.score} points!</h2>
-        {this.record(this.props.ranks)}
+        {this.props.score > x[x.length-1].score ? this.record(this.props.ranks) : null}
         <h3>The best scores:</h3>
         <ul>
-        {this.props.ranks.sort().map((data, index) => {return(
+        {this.props.ranks.map((data, index) => {return(
           <li key={index}>
             <p>{data.score}</p>
             <p>{data.name}</p>
