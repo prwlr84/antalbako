7ee0c27b920e8ae9e81a39460f385c527203c650
Antal Bako <prowlersk8@gmail.com>
2021-04-23 16:07:57 +0200

    finally ranks working

--- app/javascript/abreact/actions/index.js
+++ app/javascript/abreact/actions/index.js
@@ -1,7 +1,5 @@
-// TODO: add and export your own actions
-
 export function setLang(ip) {
-  const payload = fetch(`http://api.ipstack.com/${ip}?access_key=49ceea9b1409ef674effff735b5bd&format=1`)
+  const payload = fetch(`http://api.ipstack.com/${ip}?access_key=49ceea9b1409ef0c0674effff735b5bd&format=1`)
     .then(r=>r.json())
     .then(json => {return json.location.languages[0].code});
   return {
