71874f77cf25a01981b417315474bbbf85d71688
Antal Bako <60609555+prwlr84@users.noreply.github.com>
2021-05-07 18:18:55 +0200

    Update connect.jsx

--- app/javascript/abreact/components/connect.jsx
+++ app/javascript/abreact/components/connect.jsx
@@ -30,7 +30,7 @@ class Connect extends Component {
     const subject = document.querySelector('input[name="subject"]').value;
     const message = document.querySelector('textarea[name="message"]').value;
     console.log('Thank you for your mail!');
-    form.action = `http://localhost:3000/mail?subject=${subject}&name=${name}&email=${email}&message=${message}`;
+    form.action = `http://antalbako.codes/mail?subject=${subject}&name=${name}&email=${email}&message=${message}`;
   }
 
   render(){
