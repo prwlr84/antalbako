9a34c5c2817ac048e4caca3682b9a3db2a9c0024
Antal Bako <antal.bako@tradelink.co>
2023-09-27 12:00:52 +0200

    Upgrade

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  Rails.application.credentials.secret_key_base ||= ENV['SECRET_KEY_BASE']
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
